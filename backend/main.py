d9985916264a8916b12c7610ed6bf15cd3de7803
Smyja <akposlive59@gmail.com>
2023-12-31 12:08:08 +0100

    switched to fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 import csv
-import json
-import time
+from fastapi import FastAPI, File, UploadFile
+
 from simplejustwatchapi.justwatch import search
 from service_codes import service_codes
+
+app = FastAPI()
 
 def get_imdb_watchlist(file_path):
     watchlist = []
@@ -15,77 +17,46 @@ def get_imdb_watchlist(file_path):
             except ValueError:
                 year = None
             watchlist.append({'title': title, 'year': year})
-    #return 3 items
+    # Return 3 items
     return watchlist[:3]
 
+@app.post("/watchlist_providers")
+async def get_watchlist_providers(file: UploadFile = File(...)):
+    # Save the uploaded CSV file temporarily
+    with open("temp.csv", "wb") as f:
+        f.write(file.file.read())
 
-def main():
-
-    imdb_user_id = "ur68581805"
-
-    csv_file_path = 'ratings.csv'
-
+    # Get watchlist from the uploaded CSV
+    csv_file_path = "temp.csv"
     watchlist = get_imdb_watchlist(csv_file_path)
-
-    print(f"Movies and TV shows on your IMDb watchlist (User ID: {imdb_user_id}):")
-
     results = []
 
-
     for item in watchlist:
-
         search_results = search(item['title'], "US", "en", 5, True)
 
         if search_results:
-
             item_data = search_results[0]
-
             providers = []
 
             for offer in item_data.offers:
-
                 if offer.monetization_type.lower() == 'flatrate':
-
-                    provider_name = service_codes.get(offer.technical_name, 'Unknown')
-
-                    providers.append({'name': provider_name, 'url': offer.url})
-
+                    provider_name = service_codes.get(offer.name, 'Unknown')
+                    providers.append({'name': offer.name, 'url': offer.url})
                 elif offer.monetization_type.lower() in ['rent', 'buy']:
-
-                    provider_name = service_codes.get(offer.technical_name, 'Unknown')
-
-                    providers.append({'name': provider_name, 'url': offer.url, 'price': offer.price_string})
-
+                    provider_name = service_codes.get(offer.name, 'Unknown')
+                    providers.append({'name': offer.name, 'url': offer.url, 'price': offer.price_string})
 
             # Remove duplicates
-
             providers = [dict(t) for t in {tuple(d.items()) for d in providers}]
-
             poster_url = f"{item_data.poster}" if item_data.poster else None
 
-
             # Add item information to the results list
-
             results.append({
-
                 'title': item['title'],
-
                 'year': item['year'],
-
                 'providers': providers,
-
                 'poster_url': poster_url
-
             })
 
-
-    # Export the results as a JSON file
-
-    with open('watchlist_providers.json', 'w', encoding='utf-8') as json_file:
-
-        json.dump(results, json_file, ensure_ascii=False, indent=4)
-
-
-if __name__ == "__main__":
-
-    main()
+    # Return the results as JSON response
+    return {"watchlist_providers": results}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,21 @@
-import { Button, Group } from "@mantine/core";
+import { useState } from 'react';
+import { FileButton, Button, Group, Text } from '@mantine/core';
 
 export default function IndexPage() {
+  const [file, setFile] = useState<File | null>(null);
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <>
+      <Group justify="center">
+        <FileButton onChange={setFile} accept="image/png,image/jpeg">
+          {(props) => <Button {...props}>Upload image</Button>}
+        </FileButton>
+      </Group>
+
+      {file && (
+        <Text size="sm" ta="center" mt="sm">
+          Picked file: {file.name}
+        </Text>
+      )}
+    </>
   );
 }
